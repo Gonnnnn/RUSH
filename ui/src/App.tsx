7f67e78a82e5df4834747b02673c9ae3a27d92b1
kim.geon <kim.geon@yanolja.com>
2024-08-01 00:53:07 +0900

    Update the logo and add a loading with logo when entering the app

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,17 +1,59 @@
+import { useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Box } from '@mui/material';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import dayjs from 'dayjs';
 import 'dayjs/locale/ko';
 import AppRoutes from './Routes';
+import Logo from './assets/logo.svg';
 
 dayjs.locale('ko');
 
-const App = () => (
-  <LocalizationProvider dateAdapter={AdapterDayjs}>
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
-  </LocalizationProvider>
-);
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [fadeOut, setFadeOut] = useState(false);
+  const fadeOutTimeMillis = 1000;
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, fadeOutTimeMillis);
+
+    setFadeOut(true);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </LocalizationProvider>
+      {loading && (
+        <Box
+          position="fixed"
+          top={0}
+          left={0}
+          width="100%"
+          height="100%"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          bgcolor="#ffffff" // or any color you prefer
+          zIndex={9999}
+          sx={{
+            opacity: fadeOut ? 0 : 1,
+            transition: `opacity ${fadeOutTimeMillis}ms cubic-bezier(0.6, 0, 1, 1)`,
+          }}
+          onClick={() => setLoading(false)}
+        >
+          <img src={Logo} alt="logo" width={256} />
+        </Box>
+      )}
+    </>
+  );
+};
 export default App;
