53a5e1051a4dacfed12613703a2ce660077cd2be
kim.geon <kim.geon@yanolja.com>
2024-08-06 01:03:41 +0900

    Set cookie with more options and check auth for visibilitychange and pageshow

--- ui/src/AuthContext.tsx
+++ ui/src/AuthContext.tsx
@@ -1,4 +1,6 @@
 import { createContext, ReactNode, useCallback, useContext, useEffect, useMemo, useState } from 'react';
+import Cookies from 'js-cookie';
+import { debounce } from 'lodash';
 import { checkAuth, signIn } from './client/http';
 
 interface AuthContextType {
@@ -18,28 +20,61 @@ const cookieName = 'rush-auth';
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [authenticated, setAuthenticated] = useState(false);
 
+  const verifyAuth = useCallback(async () => {
+    try {
+      // TODO(#105): Refresh the token if it's expired.
+      const isAuthenticated = await checkAuth();
+      setAuthenticated(isAuthenticated);
+    } catch (error) {
+      // TODO(#65): Handle this error globally.
+      // eslint-disable-next-line no-console
+      console.error('Error verifying authentication:', error);
+      logout();
+    }
+  }, []);
+
+  const debouncedVerifyAuth = useMemo(() => debounce(verifyAuth, 100, { leading: true, trailing: true }), [verifyAuth]);
+
   useEffect(() => {
-    const verifyAuth = async () => {
-      try {
-        const isAuthenticated = await checkAuth();
-        setAuthenticated(isAuthenticated);
-      } catch (error) {
-        // TODO(#65): Handle this error globally.
-        // eslint-disable-next-line no-console
-        console.error('Error verifying authentication:', error);
-        logout();
+    debouncedVerifyAuth();
+
+    const handleVisibilityChange = () => {
+      // When the tab is visible, verify auth.
+      if (!document.hidden) {
+        debouncedVerifyAuth();
       }
     };
 
-    verifyAuth();
-  }, []);
+    const handlePageShow = (event: PageTransitionEvent) => {
+      // When it's not the first load, verify auth.
+      if (event.persisted) {
+        debouncedVerifyAuth();
+      }
+    };
+
+    document.addEventListener('visibilitychange', handleVisibilityChange);
+    window.addEventListener('pageshow', handlePageShow);
+
+    return () => {
+      document.removeEventListener('visibilitychange', handleVisibilityChange);
+      window.removeEventListener('pageshow', handlePageShow);
+      debouncedVerifyAuth.cancel();
+    };
+  }, [debouncedVerifyAuth]);
 
   const login = useCallback(
     async (token: string) => {
       if (token) {
         try {
           const cookie = await signIn(token);
-          document.cookie = `${cookieName}=${cookie}`;
+          Cookies.set(cookieName, cookie, {
+            // Longer than the token expiration time so that it'll be sent again and refreshed.
+            expires: 30,
+            secure: import.meta.env.VITE_ENV !== 'local',
+            domain: import.meta.env.VITE_SERVER_ENDPOINT,
+            sameSite: import.meta.env.VITE_ENV === 'local' ? 'None' : 'Strict',
+            path: '/',
+          });
           setAuthenticated(true);
         } catch (error) {
           // TODO(#65): Handle this error globally.
@@ -53,7 +88,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   );
 
   const logout = async () => {
-    document.cookie = `${cookieName}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+    Cookies.remove(cookieName, { path: '/' });
 
     // Set timer to reload the page just to make users feel like they are logged out.
     // That's why the cookie is removed before the timer is initialized.
